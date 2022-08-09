af9ff606baa8614fc36b21722b9fc641c659b6fa
Niv Caspi <niv0caspi@gmail.com>
2022-08-10 00:26:04 +0300

    updates

--- lissan.org/app.py
+++ lissan.org/app.py
@@ -4,14 +4,14 @@ import pyrebase
 
 
 config = {
-  "apiKey": "AIzaSyD-suLqhR2ikGog6OiAXgVqoYouTWa5tE8",
-  "authDomain": "individual-project-7e24c.firebaseapp.com",
-  "projectId": "individual-project-7e24c",
-  "storageBucket": "individual-project-7e24c.appspot.com",
-  "messagingSenderId": "640220452831",
-  "appId": "1:640220452831:web:f9dcabe78cbd0f41a4966a",
-  "measurementId": "G-HWRB93R0M8",
-  "databaseURL": "https://individual-project-7e24c-default-rtdb.firebaseio.com"
+  "apiKey": "AIzaSyBXMcC8_EtUT0nozIWQPCbJ10rzdHNwMJQ",
+  "authDomain": "case-study-project-3d992.firebaseapp.com",
+  "databaseURL": "https://case-study-project-3d992-default-rtdb.firebaseio.com",
+  "projectId": "case-study-project-3d992",
+  "storageBucket": "case-study-project-3d992.appspot.com",
+  "messagingSenderId": "255120934931",
+  "appId": "1:255120934931:web:f21c9ea3e89f2522d617e6",
+  "measurementId": "G-LJQ8B3589M"
 }
 
 
@@ -40,20 +40,51 @@ def shop():
 def grades():
     return render_template('lissan-for-students.org/אודות/index.html')
 
+@app.route("/for-students/forum", methods = ['POST', 'GET'])
+def forum():
+
+    comm = db.child("Comments").get().val()
+    if request.method == 'POST':
+        name = request.form["name"]
+        content = request.form["content"]
+        date = request.form["date"]        
+        comment = {"name": name, "date": date, "content": content}
+        db.child("Comments").push(comment)
+        comm = db.child("Comments").get().val()
+        return render_template("lissan-for-students.org/עדכונים/index.html", c = comm)
+    
+    return render_template("lissan-for-students.org/עדכונים/index.html", c = comm)
+    
+
 @app.route('/for-students/login', methods = ['POST', 'GET'])
 def login():
-    """
     error = ""
     if request.method == 'POST':
-        email = request.form['email-signin']
-        password = request.form['password-signin']
+        email = request.form['lemail']
+        password = request.form['lpassword']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('home'))
+            print("Way To Go!")
+            return redirect(url_for('student'))
         except:
             error = "Authentication failed"
             return render_template("lissan-for-students.org/Login/index.html")
-    """
+    return render_template("lissan-for-students.org/Login/index.html")
+    
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['semail']
+        password = request.form['spassword']
+        name = request.form['susername']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            user = {"name": name, "email": email, "password": password}
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            return redirect(url_for('student'))
+        except: 
+            return render_template("lissan-for-students.org/Login/index.html")
     return render_template("lissan-for-students.org/Login/index.html")
 
 
